8554c33df13447cc86df3d31b871bee7b46d5454
yussif mohammed <43506617+yussifm@users.noreply.github.com>
2021-08-13 02:30:12 +0000

    Quiz

--- lib/Pages/Quiz_page.dart
+++ lib/Pages/Quiz_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_totu/Service/QuesitionBrain.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({Key? key}) : super(key: key);
@@ -8,8 +9,119 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  List<Icon> scorekeeper = [];
+  int qNum = 0;
+
+  void generateQnA() {
+    if (qNum < QuizBrain().getQuLen() - 1) {
+      qNum += 1;
+    } else if (qNum >= QuizBrain().getQuLen() - 1) {
+      qNum = 0;
+      scorekeeper = [];
+    }
+  }
+
+  void checkAnswer(bool userInput) {
+    bool correctAns = QuizBrain().getAnswer(qNum);
+
+    setState(() {
+      if (userInput == correctAns) {
+        scorekeeper.add(
+          Icon(
+            Icons.check,
+            color: Colors.green,
+            size: 30.0,
+          ),
+        );
+      } else {
+        scorekeeper.add(
+          Icon(
+            Icons.close_rounded,
+            color: Colors.red,
+            size: 30.0,
+          ),
+        );
+      }
+      generateQnA();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: Padding(
+      padding: EdgeInsets.fromLTRB(10, 30, 10, 0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            flex: 4,
+            child: Center(
+              child: Text(
+                QuizBrain().getQuestions(qNum),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30.0,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                style: ButtonStyle(
+                  elevation: MaterialStateProperty.all(4.0),
+                  backgroundColor: MaterialStateProperty.all(Colors.green),
+                ),
+                child: Text(
+                  "True",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                ),
+                onPressed: () {
+                  checkAnswer(true);
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                style: ButtonStyle(
+                  elevation: MaterialStateProperty.all(4.0),
+                  backgroundColor: MaterialStateProperty.all(
+                    Colors.red,
+                  ),
+                ),
+                child: Text(
+                  "False",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                ),
+                onPressed: () {
+                  checkAnswer(false);
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: scorekeeper,
+              ),
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_totu/Pages/Quiz_page.dart';
 
 import 'Pages/AskMe_page.dart';
 
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Ask(),
+      home: Quiz(),
     );
   }
 }
