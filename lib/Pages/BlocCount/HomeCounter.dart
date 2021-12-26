b2650d1ccdd0740519fd1fe101a8ce853704fefe
yussifm13 <yussifm13@gmail.com>
2021-12-26 00:15:46 +0000

    cubit

--- lib/Pages/BlocCount/HomeCounter.dart
+++ lib/Pages/BlocCount/HomeCounter.dart
@@ -13,65 +13,67 @@ class CounterHome extends StatefulWidget {
 class _CounterHomeState extends State<CounterHome> {
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<CounterCubit>(
-      create: (context) => CounterCubit(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Cubit Counter"),
-          centerTitle: true,
-        ),
-        backgroundColor: Colors.amber,
-        body: BlocListener<CounterCubit, CounterState>(
-          listener: (context, state) {
-            if (state.wasinCrement == true) {
-              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                content: Text("Incremet"),
-                duration: Duration(seconds: 5),
-              ));
-            } else if (state.wasinCrement == false) {
-              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                content: Text("Incremet"),
-                duration: Duration(seconds: 5),
-              ));
-            }
-          },
-          child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text("You have pushed the button this many times: "),
-                Container(
-                  child: BlocBuilder<CounterCubit, CounterState>(
-                    builder: (context, state) {
-                      return Text(
-                        state.currentState.toString(),
-                        style: Theme.of(context).textTheme.headline2,
-                      );
-                    },
-                  ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Cubit Counter"),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.amber,
+      body: BlocListener<CounterCubit, CounterState>(
+        listener: (context, state) {
+          if (state.wasinCrement == true) {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text("Incremet"),
+              duration: Duration(milliseconds: 200),
+            ));
+          } else if (state.wasinCrement == false) {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text("Decremet"),
+              duration: Duration(milliseconds: 200),
+            ));
+          }
+        },
+        child: Center(
+          child: Column(
+           
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text("You have pushed the button this many times: "),
+              Container(
+                child: BlocBuilder<CounterCubit, CounterState>(
+                  builder: (context, state) {
+                    return Text(
+                      state.currentState.toString(),
+                      style: Theme.of(context).textTheme.headline2?.copyWith(  color: Colors.deepPurple,),
+                      
+                    );
+                  },
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    FloatingActionButton(
-                      onPressed: () {
-                        BlocProvider.of<CounterCubit>(context).decrement();
-                      },
-                      child: Icon(Icons.remove),
-                      tooltip: "Decrement",
-                    ),
-                    FloatingActionButton(
-                      onPressed: () {
-                        BlocProvider.of<CounterCubit>(context).incremnet();
-                      },
-                      child: Icon(Icons.add),
-                      tooltip: "INcrement",
-                    ),
-                  ],
-                )
-              ],
-            ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).decrement();
+                    },
+                    icon: Icon(Icons.remove),
+                     color: Colors.deepPurple,
+                    tooltip: "Decrement",
+                    iconSize: 50,
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).incremnet();
+                    },
+                    icon: Icon(Icons.add),
+                    color: Colors.deepPurple,
+                    tooltip: "INcrement",
+                    iconSize: 50,
+                  ),
+                ],
+              )
+            ],
           ),
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'Pages/BlocCount/cubit/counter_cubit.dart';
 import 'Pages/home.dart';
 
 void main() {
@@ -10,13 +12,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Tutorials',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: Color.fromRGBO(6, 7, 23, 1),
-        primaryColor: Color.fromRGBO(6, 8, 23, 1),
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Flutter Tutorials',
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: Color.fromRGBO(6, 7, 23, 1),
+          primaryColor: Color.fromRGBO(6, 8, 23, 1),
+        ),
+        home: ImageDemo(),
       ),
-      home: ImageDemo(),
     );
   }
 }
